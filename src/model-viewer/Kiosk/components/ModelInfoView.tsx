1acc16e3fafe3fcfb8ecfca94a4d39af9b9e24b5
Joakim Larsson <joakim.larsson2@helsingborg.se>
2022-03-28 14:39:01 +0200

    model id in querystring

--- src/model-viewer/Kiosk/components/ModelInfoView.tsx
+++ src/model-viewer/Kiosk/components/ModelInfoView.tsx
@@ -3,17 +3,23 @@ import { Model } from "@app/model-viewer/types";
 import { Grid, Typography } from "@mui/material";
 import QRCode from "react-qr-code";
 
+const createModelLink = (id: string) => {
+  const url = new URL(document.location.href);
+  url.searchParams.set("model_id", id);
+  return url.href;
+};
+
 export default function ModelInfoView({ model }: { model: Model }) {
   const {
     id,
     title,
     school: { label },
   } = model;
-
+  const link = createModelLink(id);
   return (
     <Grid container spacing={2}>
       <Grid item>
-        <QRCode size={64} value={document.location.href + id} />
+        <QRCode size={64} value={link} />
       </Grid>
       <Grid item>
         <Typography variant="h6" component="h3">
