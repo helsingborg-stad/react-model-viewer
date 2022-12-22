c6e87ea2e01abc7b60a4d0abd072c18f5070cbb1
Jakob Skote <jakob@untold.garden>
2022-12-22 23:54:57 +0100

    Update craco.config.js

--- craco.config.js
+++ craco.config.js
@@ -5,7 +5,7 @@ const CracoAlias = require("craco-alias");
 module.exports = {
   devServer: {
     headers: {
-      "Access-Control-Allow-Origin": "https://barnens-h22.multi.test",
+      "Access-Control-Allow-Origin": "*",
       "Access-Control-Allow-Methods": "GET, POST, PUT, DELETE, PATCH, OPTIONS",
       "Access-Control-Allow-Headers":
         "X-Requested-With, content-type, Authorization",
