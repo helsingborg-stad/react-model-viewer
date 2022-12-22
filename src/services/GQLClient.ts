8f6ee8c439120c3b58d17d3b958de10119bb9fb5
Jakob Skote <jakob@untold.garden>
2022-12-22 18:40:28 +0100

    Added engines to packages

--- src/services/GQLClient.ts
+++ src/services/GQLClient.ts
@@ -10,6 +10,6 @@ export default new ApolloClient({
 });
 
 
-  //uri: "",//"""https://modul-test.helsingborg.io/barnens-h22/graphql",
+  // uri: "",//"""https://modul-test.helsingborg.io/barnens-h22/graphql",
   // uri: my_current_url split at '=' and take the second part
-  //split my_current_url at '=' and take the second part
+  // split my_current_url at '=' and take the second part
